42d27d769eef67102b8d647851fa09a5e2fdda7f
xinmeng.wang <xinmeng.wang@nio.com>
2023-07-12 15:03:24 +0800

    Update: add more customer title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
+  title: "Comical ChatGPT Web",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
     title: "Comical ChatGPT Web",
