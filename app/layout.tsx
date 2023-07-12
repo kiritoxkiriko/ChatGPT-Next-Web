be8a6294a49ca920cd972d5468a9f61f7cc2e523
xinmeng.wang <xinmeng.wang@nio.com>
2023-07-12 15:03:24 +0800

    Update: add more customer title

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { getServerSideConfig } from "./config/server";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
+  title: "Comical ChatGPT Web",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
